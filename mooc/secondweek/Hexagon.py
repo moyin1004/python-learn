b760867c02e51f46d398dbdd41202803efe4f49c
moyin1004 <moyin1004@qq.com>
2019-04-01 22:01:32 +0800

    creat secondweek

---
+++ mooc/secondweek/Hexagon.py
@@ -0,0 +1,12 @@
+import turtle as t
+t.setup(600, 400, 200, 200)
+t.penup()
+t.left(60)
+t.fd(30)
+t.pendown()
+t.right(60)
+t.pensize(2)
+for i in range(6):
+    t.right(60)
+    t.fd(80)
+t.done()
---
+++ mooc/secondweek/test.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.setup(800, 400, 0, 0)
+turtle.goto(100, 100)
+turtle.goto(100, -100)
+turtle.goto(-100, -100)
+turtle.goto(-100, 100)
+turtle.goto(0, 0)
+turtle.done()
